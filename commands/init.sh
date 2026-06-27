3796effb21f1771b9fc87f576ff33e69e617d3fc
Luka Tchelidze <lukachelidze3008@gmail.com>
2026-06-27 17:36:42 +0400

    feat: add systemd service template and generate on init

--- commands/init.sh
+++ commands/init.sh
@@ -17,10 +17,13 @@ cmd_init() {
     read -rp "  App name: " APP_NAME
     read -rp "  Server host: " SERVER_HOST
     read -rp "  Server user: " SERVER_USER
+    read -rp "  Deploy path [/var/www/${APP_NAME}]: " input_deploy_path
     read -rp "  Git repo: " GIT_REPO
     read -rp "  DB user: " DB_USER
     read -rp "  DB name: " DB_NAME
     read -rp "  Release module: " RELEASE_MODULE
+
+    DEPLOY_PATH="${input_deploy_path:-/var/www/${APP_NAME}}"
 
     cp "${HAWK_ROOT}/templates/hawk.conf.example" hawk.conf
 
@@ -32,13 +35,24 @@ cmd_init() {
         -e "s|__DB_USER__|${DB_USER}|g" \
         -e "s|__DB_NAME__|${DB_NAME}|g" \
         -e "s|__RELEASE_MODULE__|${RELEASE_MODULE}|g" \
+        -e "s|__DEPLOY_PATH__|${DEPLOY_PATH}|g" \
         hawk.conf
 
     rm -f hawk.conf.bak
 
+
+    sed \
+        -e "s|__APP_NAME__|${APP_NAME}|g" \
+        -e "s|__SERVER_USER__|${SERVER_USER}|g" \
+        -e "s|__DEPLOY_PATH__|${DEPLOY_PATH}|g" \
+        "${HAWK_ROOT}/templates/hawk.service.tmpl" > "${APP_NAME}.service"
+
     log_success "hawk.conf created"
+    log_success "${APP_NAME}.service created"
     print_info "Next steps:"
     print_info "  Review hawk.conf and adjust any values"
+    print_info "  Copy ${APP_NAME}.service to /etc/systemd/system/ on your server"
+    print_info "  Then run: systemctl enable ${APP_NAME} && systemctl start ${APP_NAME}"
     print_info "  Run 'hawk doctor' to verify your setup"
 }
 
